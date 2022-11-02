42403d16148c877b116f3b3c50cab7718ca35a0f
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-02 16:52:47 +0100

    valori ok in console

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const prov = document.getElementById("riquadro");
-
+let x;
 
 
 for(let i = 1 ; i < 101 ; i++ ) {
     if (i % 3 === 0 ) {
         prov.innerHTML += `Fizz`;
+        x = `Fizz`;
     } else if(i % 5 === 0){
         prov.innerHTML += `bizz`;
-    } else { prov.innerHTML += i; }
-    console.log(i);
+        x = `bizz`;
+    } else { 
+        prov.innerHTML += i;
+        x = i;
+     }
+    console.log(x);
 }
 
 
